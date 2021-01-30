58a76b9cc2dd2f348edd830e0aab943edc3912aa
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-30 04:33:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,8 @@ def monopoly_with_third_tier
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad => {}
+        :reading_railroad => {},
+        :pennsylvania_railroad => {}
       }
     }
   }
